6e3ab35ff3d14c98bfef54276820be5d17b61728
joao <joao-fischer@hotmail.com>
2025-03-08 23:30:27 -0300

    :art: style(ButtonLogout): enhance button styling and improve logout functionality

--- src/components/ButtonLogout.tsx
+++ src/components/ButtonLogout.tsx
@@ -1,18 +1,22 @@
 'use client'
 
-import { signOut } from "next-auth/react"
+import { signOut } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 
-export default function ButtonLogout(){
-	const router = useRouter()
+export default function ButtonLogout() {
+  const router = useRouter()
 
-	async function logout() {
-		await signOut({
-			redirect: false
-		})
+  async function logout() {
+    await signOut({ redirect: false })
+    router.replace('/')
+  }
 
-		router.replace('/')
-	}
-
- return <button onClick={logout} className="p-2 w-40 border border-[var(--foreground)] rounded-md">Sair</button>
+  return (
+    <button
+      onClick={logout}
+      className="p-2 px-4 border border-white rounded-md text-orange-500 bg-white hover:bg-orange-600 hover:text-white transition"
+    >
+      Sair
+    </button>
+  )
 }
