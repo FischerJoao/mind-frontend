a5dcce09833d16bddab604dbf1e813773784705a
joao <joao-fischer@hotmail.com>
2025-03-08 21:50:18 -0300

    :sparkles: feat(auth): implement credentials provider for user authentication

--- src/app/API/auth/[...nextauth]/route.ts
+++ src/app/API/auth/[...nextauth]/route.ts
@@ -1,7 +1,55 @@
-import NextAuth from "next-auth";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import NextAuth, { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 
-const handler = NextAuth({
+const nextAuthOptions: NextAuthOptions = {
+	providers: [
+		CredentialsProvider({
+			name: 'credentials',
+			credentials: {
+				email: { label: 'email', type: 'text' },
+				password: { label: 'password', type: 'password' }
+			},
 
-})
+			async authorize(credentials, req) {
+				const response = await fetch('http://localhost:3000/auth/login', {
+					method: 'POST',
+					headers: {
+						'Content-type': 'application/json'
+					},
+					body: JSON.stringify({
+						email: credentials?.email,
+						password: credentials?.password
+					})
+				})
 
-export { handler as GET, handler as POST };
+				const user = await response.json()
+
+				if (user && response.ok) {
+					return user
+				}
+
+				return null
+			},
+		})
+	],
+	pages: {
+		signIn: '/'
+	},
+	callbacks: {
+		async jwt({ token, user }) {
+			user && (token.user = user)
+			return token
+		},
+		async session({ session, token }) {
+			session.user = token.user as any;  // ✅ Agora session mantém a estrutura correta
+			return session;
+		}
+	}
+}
+
+const handler = NextAuth(nextAuthOptions)
+
+export { handler as GET, handler as POST, nextAuthOptions }
